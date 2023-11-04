728e94e7a5fe1855686cce82f69274a9af9bf41c
Никита <99522484+kwttr@users.noreply.github.com>
2023-11-04 12:07:27 +0700

    Arendator

---
+++ Arenda/Controllers/ArendatorController.cs
@@ -0,0 +1,27 @@
+﻿using Arenda.Data;
+using Arenda.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arenda.Controllers
+{
+    public class ArendatorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ArendatorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            ArendatorViewModel arendatorVM = new ArendatorViewModel()
+            {
+                LegalEntities = _db.LegalEntities,
+                PhysicalPersons = _db.PhysicalPersons
+            };
+
+            return View(arendatorVM);
+        }
+    }
+}
---
+++ Arenda/Models/ViewModel/ArendatorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Arenda.Models.ViewModel
+{
+    public class ArendatorViewModel
+    {
+        public IEnumerable<LegalEntity> LegalEntities { get; set; }
+        public IEnumerable<PhysicalPerson> PhysicalPersons { get; set; }
+    }
+}
