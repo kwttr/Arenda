1d76d8457162a1d486e01124698b66f727fd36ba
Kerbix <99522484+kwttr@users.noreply.github.com>
2024-01-02 14:29:11 +0700

    Edit

--- Arenda/Migrations/20240102060018_rentedPremises.Designer.cs
+++ Arenda/Migrations/20240102060018_rentedPremises.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Arenda.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Arenda.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240102060018_rentedPremises")]
+    partial class rentedPremises
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
@@ -351,7 +354,7 @@ namespace Arenda.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
 
-                    b.Property<int?>("ContractId")
+                    b.Property<int>("ContractId")
                         .HasColumnType("INTEGER");
 
                     b.Property<int>("PremiseId")
@@ -595,7 +598,9 @@ namespace Arenda.Migrations
                 {
                     b.HasOne("Arenda.Models.Contract", "Contract")
                         .WithMany("rentedPremises")
-                        .HasForeignKey("ContractId");
+                        .HasForeignKey("ContractId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
 
                     b.HasOne("Arenda.Models.Premise", "Premise")
                         .WithMany()
---
+++ Arenda/Migrations/20240102060445_null.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arenda.Migrations
+{
+    /// <inheritdoc />
+    public partial class @null : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RentedPremises_Contracts_ContractId",
+                table: "RentedPremises");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractId",
+                table: "RentedPremises",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RentedPremises_Contracts_ContractId",
+                table: "RentedPremises",
+                column: "ContractId",
+                principalTable: "Contracts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RentedPremises_Contracts_ContractId",
+                table: "RentedPremises");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContractId",
+                table: "RentedPremises",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RentedPremises_Contracts_ContractId",
+                table: "RentedPremises",
+                column: "ContractId",
+                principalTable: "Contracts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
